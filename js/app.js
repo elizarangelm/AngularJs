a2aca6933af0585db0009bb362e3f881a8a0db5c
Elizabeth Rangel Monsalve <elizarangelm24@gmail.com>
2022-01-11 14:51:00 -0500

    aumentar el valor de la variable amount con un click

--- js/app.js
+++ js/app.js
@@ -1,2 +1,16 @@
 var app = angular.module('expensesApp', []);
+//cuando se corren algoritmos de verificacion se leen como string
+app.controller('ExpensesViewController', ['$scope', function ($scope) {
+    //$scope.name = "Elizabeth";
+    $scope.expense = {
+        description: 'food',
+        amount: 10
+    };
+    $scope.phrase = 'The sky is blue'
 
+    //incrementar un valor con la funcion de click
+    $scope.increaseAmount = function () {
+        $scope.expense.amount++;
+    }
+
+}]);
